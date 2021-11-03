3a49e6c7c3a7349c8c77cfaa5a8a50fe0ffb0568
Nikita <ussnikita209@mail.ru>
2021-11-03 13:37:28 +0300

    feat: complit 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const { readdir, mkdir, copyFile } = require('fs/promises');
+const path = require('path');
 
+function copyDir() {
+  const srcPath = path.resolve(__dirname, 'files')
+  readdir(srcPath, { withFileTypes: true })
+    .then((files) => {
+      const destPath = path.resolve(__dirname, 'files-copy')
+      mkdir(destPath, { recursive: true })
+      for (const file of files) {
+        copyFile(
+          path.resolve(srcPath, file.name),
+          path.resolve(destPath, file.name)
+        )
+      }
+    }, (err) => {
+      console.log(err)
+    })
+}
+
+
+copyDir()
